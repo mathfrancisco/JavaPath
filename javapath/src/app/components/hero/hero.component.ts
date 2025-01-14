39b5cff979b9f276df04fa4d3c0d0bc47306d2c0
mathfrancisco2 <math.francisco2@gmail.com>
2025-01-14 00:49:45 -0300

    ATT

--- javapath/src/app/components/hero/hero.component.ts
+++ javapath/src/app/components/hero/hero.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import {MatIcon} from '@angular/material/icon';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-hero',
   standalone: true,
-  imports: [MatButtonModule],
+  imports: [MatButtonModule, MatIcon, RouterLink],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.css'
 })
