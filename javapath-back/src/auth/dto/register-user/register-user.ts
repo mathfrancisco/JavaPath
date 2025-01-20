d4ca4a0889913cb8e43f948c479b209247df109d
Matheus Francisco <81334745+mathfrancisco@users.noreply.github.com>
2025-01-20 09:10:33 -0300

    Update register-user.ts

--- javapath-back/src/auth/dto/register-user/register-user.ts
+++ javapath-back/src/auth/dto/register-user/register-user.ts
@@ -1,1 +1,21 @@
-export class RegisterUser {}
+import { IsEmail, IsEnum, IsNotEmpty, IsString, MinLength } from 'class-validator';
+
+export class RegisterUser {
+  @IsEnum(['student', 'instructor'])
+  role: 'student' | 'instructor';
+
+  @IsString()
+  @IsNotEmpty()
+  fullName: string;
+
+  @IsString()
+  @IsNotEmpty()
+  username: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @MinLength(6)
+  password: string;
+}
