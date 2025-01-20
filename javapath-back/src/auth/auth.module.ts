0066205cec586c052f6d83bf09bde0c52b6868db
Matheus Francisco <81334745+mathfrancisco@users.noreply.github.com>
2025-01-20 09:12:54 -0300

    Update auth.module.ts

--- javapath-back/src/auth/auth.module.ts
+++ javapath-back/src/auth/auth.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { DatabaseModule } from '../database/database.module';
 
 @Module({
+  imports: [
+    DatabaseModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '24h' },
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService],
+  exports: [AuthService],
 })
 export class AuthModule {}
