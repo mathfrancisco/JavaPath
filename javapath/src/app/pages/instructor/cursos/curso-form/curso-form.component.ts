fc76eaec427f2e34226bad2314fd394cb8b83eb2
Matheus Francisco <81334745+mathfrancisco@users.noreply.github.com>
2025-01-20 13:43:50 -0300

    Update curso-form.component.ts

--- javapath/src/app/pages/instructor/cursos/curso-form/curso-form.component.ts
+++ javapath/src/app/pages/instructor/cursos/curso-form/curso-form.component.ts
@@ -7,6 +7,87 @@ import { Component } from '@angular/core';
   templateUrl: './curso-form.component.html',
   styleUrl: './curso-form.component.css'
 })
-export class CursoFormComponent {
+export class CursoFormComponent implements OnInit {
+  cursoForm: FormGroup;
+  isEditMode = false;
+  selectedFileName = '';
+  private cursoId?: number;
 
+  constructor(
+    private fb: FormBuilder,
+    private instructorService: InstructorService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {
+    this.cursoForm = this.fb.group({
+      title: ['', Validators.required],
+      description: ['', Validators.required],
+      level: ['', Validators.required],
+      price: [0, [Validators.required, Validators.min(0)]],
+      duration: [0, [Validators.required, Validators.min(0)]],
+      imageUrl: ['']
+    });
+  }
+
+  ngOnInit() {
+    this.route.params.subscribe(params => {
+      if (params['id']) {
+        this.isEditMode = true;
+        this.cursoId = +params['id'];
+        this.loadCurso();
+      }
+    });
+  }
+
+  private loadCurso() {
+    if (this.cursoId) {
+      this.instructorService.getCursoById(this.cursoId).subscribe({
+        next: (curso) => {
+          this.cursoForm.patchValue(curso);
+        },
+        error: (error) => {
+          console.error('Erro ao carregar curso:', error);
+        }
+      });
+    }
+  }
+
+  onFileSelected(event: any) {
+    const file = event.target.files[0];
+    if (file) {
+      this.selectedFileName = file.name;
+      if (this.cursoId) {
+        this.instructorService.uploadMaterial(this.cursoId, file).subscribe({
+          next: (response) => {
+            this.cursoForm.patchValue({ imageUrl: response.url });
+          },
+          error: (error) => {
+            console.error('Erro no upload:', error);
+          }
+        });
+      }
+    }
+  }
+
+  onSubmit() {
+    if (this.cursoForm.valid) {
+      const cursoData = this.cursoForm.value;
+      
+      if (this.isEditMode && this.cursoId) {
+        this.instructorService.updateCurso(this.cursoId, cursoData).subscribe({
+          next: () => this.router.navigate(['/instructor/cursos']),
+          error: (error) => console.error('Erro ao atualizar curso:', error)
+        });
+      } else {
+        this.instructorService.createCurso(cursoData).subscribe({
+          next: () => this.router.navigate(['/instructor/cursos']),
+          error: (error) => console.error('Erro ao criar curso:', error)
+        });
+      }
+    }
+  }
+
+  onCancel() {
+    this.router.navigate(['/instructor/cursos']);
+  }
 }
