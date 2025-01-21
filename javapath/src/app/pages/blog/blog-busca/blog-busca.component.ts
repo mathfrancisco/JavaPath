c562604e11d0e277947d0f43d35bf8506602c2d8
Matheus Francisco <81334745+mathfrancisco@users.noreply.github.com>
2025-01-21 08:31:20 -0300

    Update blog-busca.component.ts

--- javapath/src/app/pages/blog/blog-busca/blog-busca.component.ts
+++ javapath/src/app/pages/blog/blog-busca/blog-busca.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { BlogService, Post } from './blog.service';
+import { CardBlogComponent } from './card-blog.component';
 
 @Component({
   selector: 'app-blog-busca',
@@ -7,6 +12,35 @@ import { Component } from '@angular/core';
   templateUrl: './blog-busca.component.html',
   styleUrl: './blog-busca.component.css'
 })
-export class BlogBuscaComponent {
+implements OnInit {
+  posts: Post[] = [];
+  searchTerm = '';
+  availableTags: string[] = ['Java', 'Spring', 'Angular', 'TypeScript'];
+  selectedTags: string[] = [];
 
+  constructor(private blogService: BlogService) {}
+
+  ngOnInit() {
+    this.loadPosts();
+  }
+
+  loadPosts() {
+    this.blogService
+      .getPosts(this.searchTerm, this.selectedTags)
+      .subscribe(posts => this.posts = posts);
+  }
+
+  onSearch() {
+    this.loadPosts();
+  }
+
+  toggleTag(tag: string) {
+    const index = this.selectedTags.indexOf(tag);
+    if (index === -1) {
+      this.selectedTags.push(tag);
+    } else {
+      this.selectedTags.splice(index, 1);
+    }
+    this.loadPosts();
+  }
 }
