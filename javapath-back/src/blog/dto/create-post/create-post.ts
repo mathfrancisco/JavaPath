097712cdebce7a17b14a4aba2f6baca72f9c56e3
Matheus Francisco <81334745+mathfrancisco@users.noreply.github.com>
2025-01-21 07:59:42 -0300

    Update create-post.ts

--- javapath-back/src/blog/dto/create-post/create-post.ts
+++ javapath-back/src/blog/dto/create-post/create-post.ts
@@ -1,1 +1,21 @@
-export class CreatePost {}
+// dto/create-post.dto.ts
+import { IsArray, IsBoolean, IsNotEmpty, IsOptional, IsString } from 'class-validator';
+
+export class CreatePostDto {
+  @IsString()
+  @IsNotEmpty()
+  title: string;
+
+  @IsString()
+  @IsNotEmpty()
+  content: string;
+
+  @IsBoolean()
+  @IsOptional()
+  published?: boolean;
+
+  @IsArray()
+  @IsString({ each: true })
+  @IsOptional()
+  tags?: string[];
+}
