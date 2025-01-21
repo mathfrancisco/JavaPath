e65dc140f045c54eaf27eefc41917fd70d20d2ae
Matheus Francisco <81334745+mathfrancisco@users.noreply.github.com>
2025-01-21 08:16:59 -0300

    Update comentarios.module.ts

--- javapath-back/src/comentarios/comentarios.module.ts
+++ javapath-back/src/comentarios/comentarios.module.ts
@@ -1,9 +1,15 @@
+// comentarios.module.ts
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ComentariosController } from './comentarios.controller';
 import { ComentariosService } from './comentarios.service';
+import { Comentario } from './entities/comentario.entity';
+import { Post } from '../blog/entities/post.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Comentario, Post])],
   controllers: [ComentariosController],
-  providers: [ComentariosService]
+  providers: [ComentariosService],
+  exports: [ComentariosService],
 })
 export class ComentariosModule {}
