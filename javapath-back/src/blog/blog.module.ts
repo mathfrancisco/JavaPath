6ffd6f8ffa918b61a7889938c04d4e9625d44011
Matheus Francisco <81334745+mathfrancisco@users.noreply.github.com>
2025-01-21 08:00:58 -0300

    Update blog.module.ts

--- javapath-back/src/blog/blog.module.ts
+++ javapath-back/src/blog/blog.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { BlogController } from './blog.controller';
 import { BlogService } from './blog.service';
+import { Post } from './entities/post.entity';
+import { Tag } from './entities/tag.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Post, Tag])],
   controllers: [BlogController],
-  providers: [BlogService]
+  providers: [BlogService],
 })
 export class BlogModule {}
