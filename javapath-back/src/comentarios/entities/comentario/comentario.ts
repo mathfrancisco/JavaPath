6562dca15fde6a450f486c3ed4415b896f2c9b7f
Matheus Francisco <81334745+mathfrancisco@users.noreply.github.com>
2025-01-21 08:06:30 -0300

    Update comentario.ts

--- javapath-back/src/comentarios/entities/comentario/comentario.ts
+++ javapath-back/src/comentarios/entities/comentario/comentario.ts
@@ -1,1 +1,30 @@
-export class Comentario {}
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, CreateDateColumn, UpdateDateColumn } from 'typeorm';
+import { User } from '../user/user.entity';
+import { Post } from '../blog/entities/post.entity';
+
+@Entity()
+export class Comentario {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column('text')
+  conteudo: string;
+
+  @CreateDateColumn()
+  criadoEm: Date;
+
+  @UpdateDateColumn()
+  atualizadoEm: Date;
+
+  @Column({ default: false })
+  moderado: boolean;
+
+  @ManyToOne(() => User, user => user.comentarios)
+  autor: User;
+
+  @ManyToOne(() => Post, post => post.comentarios)
+  post: Post;
+
+  @ManyToOne(() => Comentario, { nullable: true })
+  comentarioPai: Comentario;
+}
