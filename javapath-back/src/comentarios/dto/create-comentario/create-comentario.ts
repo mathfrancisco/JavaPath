955cb1cb2c642f1654b7034c93baddb69a6df744
Matheus Francisco <81334745+mathfrancisco@users.noreply.github.com>
2025-01-21 08:15:30 -0300

    Update create-comentario.ts

--- javapath-back/src/comentarios/dto/create-comentario/create-comentario.ts
+++ javapath-back/src/comentarios/dto/create-comentario/create-comentario.ts
@@ -1,1 +1,16 @@
-export class CreateComentario {}
+import { IsNotEmpty, IsOptional, IsString, IsUUID } from 'class-validator';
+
+export class CreateComentarioDto {
+  @IsString()
+  @IsNotEmpty()
+  conteudo: string;
+
+  @IsUUID()
+  @IsNotEmpty()
+  postId: string;
+
+  @IsUUID()
+  @IsOptional()
+  comentarioPaiId?: string;
+}
+
