28f185eb2bc3049889f44bfde2db2d19a26c0d18
Matheus Francisco <81334745+mathfrancisco@users.noreply.github.com>
2025-01-21 07:57:58 -0300

    Update post.ts

--- javapath-back/src/blog/entities/post/post.ts
+++ javapath-back/src/blog/entities/post/post.ts
@@ -1,1 +1,34 @@
-export class Post {}
+import { Column, CreateDateColumn, Entity, ManyToOne, OneToMany, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
+import { User } from '../user/user.entity';
+import { Tag } from './tag.entity';
+import { Comment } from './comment.entity';
+
+@Entity()
+export class Post {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  title: string;
+
+  @Column('text')
+  content: string;
+
+  @Column({ default: false })
+  published: boolean;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @ManyToOne(() => User, user => user.posts)
+  author: User;
+
+  @OneToMany(() => Comment, comment => comment.post)
+  comments: Comment[];
+
+  @OneToMany(() => Tag, tag => tag.posts)
+  tags: Tag[];
+}
