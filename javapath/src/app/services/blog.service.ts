fd209daec6ee70d981e2f3215f3d7a6b5e3d15e2
Matheus Francisco <81334745+mathfrancisco@users.noreply.github.com>
2025-01-21 08:28:40 -0300

    Update blog.service.ts

--- javapath/src/app/services/blog.service.ts
+++ javapath/src/app/services/blog.service.ts
@@ -1,9 +1,67 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../environments/environment';
+
+export interface Post {
+  id: string;
+  title: string;
+  content: string;
+  published: boolean;
+  createdAt: Date;
+  updatedAt: Date;
+  author: {
+    id: string;
+    name: string;
+    avatar?: string;
+  };
+  tags: string[];
+  comments: Comment[];
+}
+
+export interface Comment {
+  id: string;
+  content: string;
+  createdAt: Date;
+  author: {
+    id: string;
+    name: string;
+    avatar?: string;
+  };
+  moderado: boolean;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class BlogService {
+  private apiUrl = `${environment.apiUrl}/blog`;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getPosts(search?: string, tags?: string[]): Observable<Post[]> {
+    let url = this.apiUrl;
+    const params: any = {};
+    
+    if (search) params.search = search;
+    if (tags?.length) params.tags = tags.join(',');
+    
+    return this.http.get<Post[]>(url, { params });
+  }
+
+  getPost(id: string): Observable<Post> {
+    return this.http.get<Post>(`${this.apiUrl}/${id}`);
+  }
+
+  createPost(post: Partial<Post>): Observable<Post> {
+    return this.http.post<Post>(this.apiUrl, post);
+  }
+
+  updatePost(id: string, post: Partial<Post>): Observable<Post> {
+    return this.http.put<Post>(`${this.apiUrl}/${id}`, post);
+  }
+
+  deletePost(id: string): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
 }
