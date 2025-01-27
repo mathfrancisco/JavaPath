33b921abc7ea8fa66d5f182d99c260ecd54717a5
Matheus Francisco <81334745+mathfrancisco@users.noreply.github.com>
2025-01-27 08:23:04 -0300

    Update database.module.ts

--- javapath-back/src/database/database.module.ts
+++ javapath-back/src/database/database.module.ts
@@ -1,7 +1,31 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { SupabaseService } from './supabase/supabase.service';
 
 @Module({
-  providers: [SupabaseService]
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('SUPABASE_HOST'),
+        port: configService.get('SUPABASE_PORT'),
+        username: configService.get('SUPABASE_USER'),
+        password: configService.get('SUPABASE_PASSWORD'),
+        database: configService.get('SUPABASE_DB'),
+        entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+        synchronize: configService.get('NODE_ENV') !== 'production',
+        ssl: {
+          rejectUnauthorized: false,
+        },
+        autoLoadEntities: true,
+      }),
+    }),
+  ],
+  providers: [SupabaseService],
+  exports: [SupabaseService, TypeOrmModule],
 })
 export class DatabaseModule {}
