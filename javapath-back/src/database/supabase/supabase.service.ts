9d008de63895577672668344ff5e8855781ee345
Matheus Francisco <81334745+mathfrancisco@users.noreply.github.com>
2025-01-27 08:22:47 -0300

    Update supabase.service.ts

--- javapath-back/src/database/supabase/supabase.service.ts
+++ javapath-back/src/database/supabase/supabase.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { createClient, SupabaseClient } from '@supabase/supabase-js';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
-export class SupabaseService {}
+export class SupabaseService {
+  private supabase: SupabaseClient;
+
+  constructor(private configService: ConfigService) {
+    this.supabase = createClient(
+      this.configService.get<string>('SUPABASE_URL'),
+      this.configService.get<string>('SUPABASE_SECRET_KEY'),
+      {
+        auth: {
+          autoRefreshToken: false,
+          persistSession: false
+        },
+        db: {
+          schema: 'public'
+        }
+      }
+    );
+  }
+
+  get client() {
+    return this.supabase;
+  }
+}
